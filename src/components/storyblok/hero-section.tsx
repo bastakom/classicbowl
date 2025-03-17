9a798a9b1b41fa1ec45ed734fa994eabc0d2190d
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-17 11:26:30 +0100

    rework hamburger menu

--- src/components/storyblok/hero-section.tsx
+++ src/components/storyblok/hero-section.tsx
@@ -7,7 +7,6 @@ import { LinkTypes } from "@/types/IfLinkInterface";
 import { Button } from "../ui/button";
 import { useParams } from "next/navigation";
 
-
 interface HeroProps {
   blok: {
     title: string;
@@ -31,7 +30,6 @@ interface HeroProps {
 }
 
 export const HeroSection = ({ blok }: HeroProps) => {
-
   const router = useParams();
 
   return (
@@ -40,17 +38,14 @@ export const HeroSection = ({ blok }: HeroProps) => {
       className={`h-full w-full flex flex-col justify-center mx-auto`}
     >
       <div
-        className={`relative ${blok?.video
+        className={`relative ${
+          blok?.video
             ? "h-full"
             : blok.small_hero
-              ? "h-[50vh] lg:min-h-[50vh]"
-              : "h-full lg:min-h-[90vh]"
-          } justify-center flex items-center ${blok.frame && "container-section mt-20"
-          }`}
-            ? "min-h-[50vh] lg:min-h-[50vh]"
-            : "min-h-[90vh] lg:min-h-[90vh]"
+            ? "h-[50vh] lg:min-h-[50vh]"
+            : "min-h-[90vh]"
         } justify-center flex items-center ${
-          blok.frame && "container mx-auto px-4"
+          blok.frame && "container-section mt-20  mx-auto px-4"
         }`}
       >
         <div
