17eaa612557ebdc953165cd4630fc6e1af632fa4
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-24 15:52:39 +0100

    styling korr

--- src/components/storyblok/text-btn.tsx
+++ src/components/storyblok/text-btn.tsx
@@ -3,14 +3,14 @@ import { render } from "storyblok-rich-text-react-renderer";
 
 export const TextBtn = ({ blok }: any) => {
   return (
-    <div className="flex flex-col lg:flex-row mx-auto lg:w-[90%] gap-10 p-6 lg:p-10">
+    <div className="flex flex-col items-center lg:items-start lg:flex-row mx-auto lg:w-[90%] gap-10 p-6 lg:p-10">
       <div className="lg:w-[50%] text-block-content">
         {render(blok?.content)}
       </div>
       <Link
         href={blok?.link?.cached_url}
         style={{ backgroundColor: blok?.button_bg.color }}
-        className="text-btn rounded-[70px] h-[60px]"
+        className="text-btn rounded-[70px] h-[60px] w-[60%] lg:w-[15%]"
       >
         {blok?.link_title}
       </Link>
