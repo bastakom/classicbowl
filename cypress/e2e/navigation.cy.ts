8aa8e9bff5eb5887a9558cd057f19e13fb8105cf
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-24 12:59:06 +0100

    changed url

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,13 +1,13 @@
 describe("navigation tests", () => {
   it("test opening and closing sidebar", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit(`${process.env.baseUrl || "http://localhost:3000/"}`);
     cy.get(".hamburger-react").click();
     cy.get('[data-id="close-sidebar-menu"]').click();
   });
 
   it("tests navigation mobile", () => {
     cy.viewport(375, 812);
-    cy.visit("http://localhost:3000/");
+    cy.visit(`${process.env.baseUrl || "http://localhost:3000/"}`);
     cy.get(".hamburger-react").click();
     cy.get('[data-id="close-sidebar-menu"]').click();
   });
