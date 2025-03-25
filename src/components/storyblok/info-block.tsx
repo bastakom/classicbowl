21e0d9671145adda65550200a4e404dbe473eeab
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-03-25 11:42:59 +0100

    small styling addition

--- src/components/storyblok/info-block.tsx
+++ src/components/storyblok/info-block.tsx
@@ -7,8 +7,16 @@ export const InfoBlock = ({ blok }: any) => {
     <div className="flex flex-col items-center mt-10 lg:mt-0 lg:py-10 lg:mb-16">
       <div className="p-6 lg:w-[50%] flex flex-col gap-12">
         <h2 className="uppercase text-center">{blok.title}</h2>
-        <div>{render(blok.content)}</div>
-        <div className="flex items-center gap-2 justify-center text-[#1E40AF]">
+        <div className={`${blok.text_center && "text-center"}`}>
+          {render(blok.content)}
+        </div>
+        <div
+          className={`${
+            blok.link_title == ""
+              ? "hidden"
+              : "flex items-center gap-2 justify-center text-[#1E40AF]"
+          }`}
+        >
           <Link href={blok.link.cached_url} className="font-extrabold">
             {blok.link_title}
           </Link>
