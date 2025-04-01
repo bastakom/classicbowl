9f44399700b631cc26be5d6d47cfc8849fa39ed0
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-01 16:59:36 +0200

    added mobile styling

--- src/components/info-and-form.tsx
+++ src/components/info-and-form.tsx
@@ -5,13 +5,13 @@ export const InfoForm = ({ blok, settings }: any) => {
   return (
     <div
       id="booking-form"
-      className="grid grid-cols-2 bg-[#660708] pt-20  pr-11 my-20"
+      className="grid lg:grid-cols-2 bg-[#660708] p-4 lg:pt-20 pt-16 lg:pr-11 my-20"
     >
-      <div className=" flex flex-col items-center text-white">
+      <div className=" flex flex-col lg:items-center text-white">
         <h2 className="text-[35px] font-extrabold uppercase italic">
           {blok.title}
         </h2>
-        <div className="mt-5 w-[50%]">{render(blok.content)}</div>
+        <div className="mt-5 lg:w-[50%]">{render(blok.content)}</div>
       </div>
 
       <Bookingform settings={settings} blok={blok} />
