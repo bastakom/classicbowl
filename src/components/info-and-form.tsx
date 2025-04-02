27ea57c33b6a98643569f64b7825bfc2150a6eee
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-02 13:47:51 +0200

    small styling fix

--- src/components/info-and-form.tsx
+++ src/components/info-and-form.tsx
@@ -7,7 +7,7 @@ export const InfoForm = ({ blok, settings }: any) => {
       id="booking-form"
       className="grid lg:grid-cols-2 bg-[#660708] p-4 lg:pt-20 pt-16 lg:pr-11 my-20"
     >
-      <div className="flex flex-col  text-white px-48">
+      <div className="flex flex-col  text-white lg:px-48">
         <h2 className="text-[35px] font-extrabold uppercase italic">
           {blok.title}
         </h2>
