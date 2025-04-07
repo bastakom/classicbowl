a04da4bb307a4a8c40360cc2e770dcfa64182984
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-07 20:45:53 +0200

    error handling

--- src/components/ui/blogg/card.tsx
+++ src/components/ui/blogg/card.tsx
@@ -16,28 +16,28 @@ export const Card = ({ event, settings }: CardProps) => {
         <div className="mt-5 lg:mt-0">
           <div className="flex gap-2">
             <span className="text-[#B71B2F] text-[22px] font-extrabold mb-2">
-              {event.content.Datum.split(" ")[0]}
+              {event?.content?.Datum?.split(" ")[0]}
             </span>
-            <span className="text-[#B71B2F] text-[22px] font-extrabold">{` - ${event.content.Time}`}</span>
+            <span className="text-[#B71B2F] text-[22px] font-extrabold">{` - ${event?.content?.Time}`}</span>
           </div>
           <h2 className="font-extrabold text-[30px] lg:text-[40px] uppercase mb-3 lg:mb-6">
             {event.name}
           </h2>
-          {event.content.event_content && (
+          {event?.content?.event_content && (
             <div className="lg:w-[70%] render-blogg-content">
-              {render(event.content.event_content)}
+              {render(event?.content?.event_content)}
             </div>
           )}
           <div className="flex items-center gap-2 uppercase font-extrabold italic text-[16px] text-[#1E40AF] mt-5 mb-5 lg:mb-0 lg:mt-10">
-            <Link href={`/${event.content.more_events_link.cached_url}`}>
-              {event.content.more_events_title}
+            <Link href={`/${event?.content?.more_events_link?.cached_url}`}>
+              {event?.content?.more_events_title}
             </Link>
             <ArrowRight />
           </div>
         </div>
         <div className="w-[345px] lg:w-[531px] h-[300px] lg:h-[500px] relative">
           <Image
-            src={event.content.image.filename || ""}
+            src={event?.content?.image?.filename || ""}
             fill
             alt={event.name}
             className="object-cover "
