e4d220a8d7bdc550a20201adf0a2a52c42fe8fd8
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-23 16:39:04 +0200

    added id to form and changed margin

--- src/components/storyblok/image-form.tsx
+++ src/components/storyblok/image-form.tsx
@@ -7,9 +7,8 @@ import { StandardForm } from "../ui/form/standard-form";
 export const ImageForm = ({ blok, settings }: any) => {
   return (
     <div
-      className={`w-[100%] bg-[#660708] p-5 lg:p-14  lg:pt-24 lg:mt-10 ${
-        blok.contact_form && "mb-10 lg:mb-20"
-      }`}
+      id="contact"
+      className={`w-[100%] bg-[#660708] p-5 lg:p-14  lg:pt-24 lg:mt-10 `}
     >
       <div className="grid grid-cols-1 lg:grid-cols-[40%_65%] w-[95%] mx-auto ">
         <div className="relative w-full h-[350px] lg:w-[100%] lg:h-[70%] lg:mt-16 mb-10 lg:mb-0">
