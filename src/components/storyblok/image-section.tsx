44ddfcdcfb7a79134faf1d9b53016c09f336b7a4
anlander <anlanderp@gmail.com>
2025-05-08 11:45:54 +0200

    main

--- src/components/storyblok/image-section.tsx
+++ src/components/storyblok/image-section.tsx
@@ -9,16 +9,14 @@ export const ImageSection = ({ blok }: any) => {
   return (
     <div
       {...storyblokEditable}
-      className={`container-section flex flex-col justify-center items-center mx-auto mt-10 lg:mt-16  ${
-        blok.bg_color?.color && "p-5 lg:p-14"
-      }`}
+      className={`container-section flex flex-col justify-center items-center mx-auto mt-10 lg:mt-16  ${blok.bg_color?.color && "p-5 lg:p-14"
+        }`}
       style={{ background: `${blok.bg_color?.color || ""}` }}
     >
       <div className=" flex flex-col justify-center items-center  w-[100%] text-center mb-5 lg:mb-0 ">
         <div
-          className={`flex flex-col gap-5 lg:max-w-[50%] justify-center ${
-            blok.text_white && "text-white"
-          }`}
+          className={`flex flex-col gap-5 lg:max-w-[50%] justify-center ${blok.text_white && "text-white"
+            }`}
         >
           {blok.sub_title && <h3>{blok.sub_title}</h3>}
           {blok.title && (
@@ -41,17 +39,15 @@ export const ImageSection = ({ blok }: any) => {
           </div>
         </div>
         <div
-          className={`w-[100%] mt-4 lg:w-[50%] lg:mt-0 h-[350px] lg:h-[545px] relative mb-4 ${
-            blok.image_right && "order-1"
-          }`}
+          className={`w-[100%] mt-4 lg:w-[50%] lg:mt-0 h-[350px] lg:h-[545px] relative mb-4 ${blok.image_right && "order-1"
+            }`}
         >
           <Image
             src={blok.image.filename || ""}
             alt={blok.title}
             fill
-            className={`object-cover ${
-              blok.image_right ? "p-0 lg:p-10 pr-0" : "p-0 lg:p-10 pl-0"
-            }`}
+            className={`object-cover ${blok.image_right ? "p-0 lg:p-10 pr-0" : "p-0 lg:p-10 pl-0"
+              }`}
           />
         </div>
       </div>
