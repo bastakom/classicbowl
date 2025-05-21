d649a6b8dcd7d6a14b0a738cded54f6f1d742359
anlander <anlanderp@gmail.com>
2025-05-21 09:12:08 +0200

    cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,18 @@ export const metadata: Metadata = {
     "Vi erbjuder allt från Bowling & Shuffleboard till Bar & Restaurang med fulla rättigheter. Välkomna!",
 };
 
+const cachedFetch = (input: any, init?: any): Promise<Response> => {
+  return fetch(input, {
+    ...init,
+    cache: "no-cache",
+  });
+};
+
 storyblokInit({
   accessToken: process.env.STORYBLOK_TOKEN,
   use: [apiPlugin],
   apiOptions: {
-    region: "eu",
+    fetch: cachedFetch,
   },
 });
 
