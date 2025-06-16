95c293b0408ef9785e7a3691460fbafa0a145569
anlander <anlanderp@gmail.com>
2025-06-16 13:28:06 +0200

    mail

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
   `;
   try {
     const { data, error } = await resend.emails.send({
-      from: "Notifikation: Kontakt - företagssidan <onboarding@resend.dev>",
+      from: "Notifikation Kontakt - företagssidan <onboarding@resend.dev>",
       to: [`${usermail}`],
       subject: `Notifikation: Customer ${name}`,
       html: messageBody,
