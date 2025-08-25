132e8e8ac1213efe48285859a40033478e56a419
anlander <anlanderp@gmail.com>
2025-08-25 10:24:08 +0200

    en

--- src/components/storyblok/menu-block.tsx
+++ src/components/storyblok/menu-block.tsx
@@ -1,19 +1,13 @@
 import { render } from "storyblok-rich-text-react-renderer";
-import { MdOutlineArrowDropDown } from "react-icons/md";
 import { useState } from "react";
-import { ContactForm } from "../ui/form/contact-form";
 import { GoPlus } from "react-icons/go";
 
 export const MenuBlock = ({ blok, settings, props }: any) => {
-  const [openDropdown, setOpenDropdown] = useState(false);
   const [selectedId, setSelectedId] = useState("");
 
-  const handleDropdown = () => {
-    setOpenDropdown(!openDropdown);
-  };
-
-  const handleSelectedId = (id: string) => {
-    setSelectedId(id);
+  const handleDropdown = (id: string) => {
+    // If clicking the same item, close it. If clicking a different item, open that one.
+    setSelectedId(selectedId === id ? "" : id);
   };
 
   return (
@@ -76,21 +70,18 @@ export const MenuBlock = ({ blok, settings, props }: any) => {
 
         {/* For mobile */}
 
-        <div className="flex flex-col lg:hidden text-white bg-[#660708] py-12 p-6 pb-20">
+        <div className="flex flex-col lg:hidden text-white bg-[#660708] w-full py-12 p-6 pb-20">
           {blok.field.map((item: any, index: number) => (
             <div className="mt-6" key={index}>
               <div
                 className="flex flex-col items-center justify-center gap-2"
-                onClick={() => {
-                  handleDropdown();
-                  handleSelectedId(item._uid);
-                }}
+                onClick={() => handleDropdown(item._uid)}
               >
                 <div className="flex justify-center items-center gap-5">
                   <div
                     className={`text-[25px] uppercase font-bold italic ${blok.bigger_field
-                        ? "text-center lg:text-start"
-                        : "text-start"
+                      ? "text-center lg:text-start"
+                      : "text-start"
                       } `}
                   >
                     {item.title}
@@ -98,7 +89,8 @@ export const MenuBlock = ({ blok, settings, props }: any) => {
 
                   <GoPlus
                     fontSize={30}
-                    className="shrink-0 text-black bg-[#F5F3F4] rounded-full p-[5px] transition-transform duration-200 "
+                    className={`shrink-0 text-black bg-[#F5F3F4] rounded-full p-[5px] transition-transform duration-200 ${selectedId === item._uid ? "rotate-45" : ""
+                      }`}
                   />
                 </div>
                 <div className="menu-content mt-2 mb-4">
@@ -108,8 +100,7 @@ export const MenuBlock = ({ blok, settings, props }: any) => {
               </div>
 
               <div
-                className={`${openDropdown && selectedId == item._uid ? "block" : "hidden"
-                  }`}
+                className={`${selectedId === item._uid ? "block" : "hidden"}`}
               >
                 {item.menu_item.map((el: any) => (
                   <div className="flex justify-between mb-6 mt-8">
