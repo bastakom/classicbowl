042a2997ba399169925b6b614a0b2f120959bc37
anlander <anlanderp@gmail.com>
2025-09-26 14:57:41 +0200

    meny

--- src/components/storyblok/lunch-block.tsx
+++ src/components/storyblok/lunch-block.tsx
@@ -2,22 +2,21 @@ import { useState } from "react";
 
 export const LunchBlock = ({ blok }: any) => {
   const [item, setItem] = useState(1);
+  console.log(blok)
 
   return (
     <div className="mt-10 lg:mt-0 flex flex-col justify-center items-center mb-20">
       <div className="flex gap-16 uppercase text-[16px] italic font-extrabold">
         <div
-          className={`cursor-pointer transition-colors ${
-            item === 1 ? "text-[#666666]" : "text-black"
-          }`}
+          className={`cursor-pointer transition-colors ${item === 1 ? "text-[#666666]" : "text-black"
+            }`}
           onClick={() => setItem(1)}
         >
           {blok.first_week}
         </div>
         <div
-          className={`cursor-pointer transition-colors ${
-            item === 2 ? "text-[#666666]" : "text-black"
-          }`}
+          className={`cursor-pointer transition-colors ${item === 2 ? "text-[#666666]" : "text-black"
+            }`}
           onClick={() => setItem(2)}
         >
           {blok.second_week}
@@ -27,31 +26,43 @@ export const LunchBlock = ({ blok }: any) => {
       <div className="mt-10 lg:mt-20 flex flex-col gap-6 text-center px-2 mx-auto lg:px-0">
         {item === 1
           ? blok.first_field.map((item: any, index: number) => (
-              <div key={index}>
-                <h3 className="text-[25px] font-bold italic uppercase">
-                  {item.title}
-                </h3>
-                <div className="text-[22px] font-semibold">{item.dish}</div>
-                <div className="mb-4">{item.description}</div>
-                <div className="text-[22px] font-semibold">
-                  {item.second_dish}
-                </div>
-                <div>{item.second_description}</div>
+            <div key={index}>
+              <h3 className="text-[25px] font-bold italic uppercase">
+                {item.title}
+              </h3>
+              <div className="text-[22px] font-semibold">{item.dish}</div>
+              <div className="mb-4">{item.description}</div>
+              <div className="text-[22px] font-semibold">
+                {item.second_dish}
               </div>
-            ))
+              <div>{item.second_description}</div>
+              {item.VEG &&
+                <div className="mt-4">
+                  <h3 className="text-[22px] font-semibold">{item.VEG}</h3>
+                  <div>{item.VEG_description}</div>
+                </div>
+              }
+            </div>
+          ))
           : blok.second_field.map((item: any, index: number) => (
-              <div key={index}>
-                <h3 className="text-[25px] font-bold italic uppercase">
-                  {item.title}
-                </h3>
-                <div className="text-[22px] font-semibold">{item.dish}</div>
-                <div className="mb-4">{item.description}</div>
-                <div className="text-[22px] font-semibold">
-                  {item.second_dish}
-                </div>
-                <div>{item.second_description}</div>
+            <div key={index}>
+              <h3 className="text-[25px] font-bold italic uppercase">
+                {item.title}
+              </h3>
+              <div className="text-[22px] font-semibold">{item.dish}</div>
+              <div className="mb-4">{item.description}</div>
+              <div className="text-[22px] font-semibold">
+                {item.second_dish}
               </div>
-            ))}
+              <div>{item.second_description}</div>
+              {item.VEG &&
+                <div className="mt-4">
+                  <h3 className="text-[22px] font-semibold">{item.VEG}</h3>
+                  <div>{item.VEG_description}</div>
+                </div>
+              }
+            </div>
+          ))}
       </div>
     </div>
   );
